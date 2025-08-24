b1169371eb049cfef476bc1fdea7aa18e3c93bb3
Rudra160113 <200001226+Rudra160113@users.noreply.github.com>
2025-08-24 13:33:35 +0530

    Remove WebBrowser import and usage in App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// Import statements
+import React from 'react';
+// import WebBrowser from 'some-path-to-WebBrowser'; // This line will be removed
+
+function App() {
+  // Code that may contain usage of WebBrowser
+  // WebBrowser.someMethod(); // This line will be removed
+
+  return (
+    <div>
+      <h1>Welcome to Vibha</h1>
+      {/* Other components */}
+    </div>
+  );
+}
+
+export default App;
