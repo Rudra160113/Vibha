6f9af8aea93baed518530ff95951ee21ee316fa7
Rudra160113 <200001226+Rudra160113@users.noreply.github.com>
2025-08-30 05:13:00 +0530

    Create useAuth.ts

---
+++ vibha-browser/hooks/useAuth.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import { supabase } from "../services/supabaseClient";
+
+export default function useAuth() {
+  const [session, setSession] = useState<any>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    const { data: listener } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => {
+      listener.subscription.unsubscribe();
+    };
+  }, []);
+
+  return session;
+}
