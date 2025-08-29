e7c470c902c9d1d17fb5a3cadb70414c0445fdb6
Rudra160113 <200001226+Rudra160113@users.noreply.github.com>
2025-08-30 05:13:46 +0530

    Create navigation.ts

---
+++ vibha-browser/constants/navigation.ts
@@ -0,0 +1,11 @@
+export const NAV_ROUTES = {
+  ROOT: "Root",
+  AUTH: "Auth",
+  MAIN: "Main",
+  LOGIN: "Login",
+  REGISTER: "Register",
+  HOME: "Home",
+  HISTORY: "History",
+  DOWNLOADS: "Downloads",
+  SETTINGS: "Settings",
+};
